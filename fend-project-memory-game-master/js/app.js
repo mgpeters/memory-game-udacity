1d61952892e0bc907bde9456415c9ab8742b5199
Matt Peters <matthew.george.peters@gmail.com>
2018-06-13 17:05:34 -0400

    shuffle functio works with cards array

--- fend-project-memory-game-master/js/app.js
+++ fend-project-memory-game-master/js/app.js
@@ -34,7 +34,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
